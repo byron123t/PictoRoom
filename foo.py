250df96fdc6ac5cb6a0870afacca9977e701c214
Takeshi Suzuki <takeshibaconsuzuki@gmail.com>
2018-02-24 17:53:43 -0600

    Janky first implementation

---
+++ foo.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+"""Server for multithreaded (asynchronous) chat application."""
+from socket import AF_INET, socket, SOCK_STREAM
+from threading import Thread
+
+
+def accept_incoming_connections():
+    """Sets up handling for incoming clients."""
+    while True:
+        client, client_address = SERVER.accept()
+        print("%s:%s has connected." % client_address)
+        addresses[client] = client_address
+        Thread(target=handle_client, args=(client,)).start()
+
+
+def handle_client(client):  # Takes client socket as argument.
+    """Handles a single client connection."""
+    clients[client] = 1
+
+    while True:
+        msg = client.recv(BUFSIZ)
+        if msg == bytes("{quit}", "utf8") or msg == b'':
+            print('%s quit' % addresses[client])
+            client.send(bytes("{quit}", "utf8"))
+            client.close()
+            del clients[client]
+            break
+        print('%s: %s' % (addresses[client], msg))
+        broadcast(msg)
+
+
+def broadcast(msg, prefix=""):  # prefix is for name identification.
+    """Broadcasts a message to all the clients."""
+
+    for sock in clients:
+        sock.send(bytes(prefix, "utf8")+msg)
+
+
+clients = {}
+addresses = {}
+
+HOST = '0.0.0.0'
+PORT = 8000
+BUFSIZ = 1024
+ADDR = (HOST, PORT)
+
+SERVER = socket(AF_INET, SOCK_STREAM)
+SERVER.bind(ADDR)
+
+if __name__ == "__main__":
+    SERVER.listen(5)
+    print("Waiting for connection...")
+    ACCEPT_THREAD = Thread(target=accept_incoming_connections)
+    ACCEPT_THREAD.start()
+    ACCEPT_THREAD.join()
+    SERVER.close()
