897d57bea080a53a26eebcb848ec3bee5476445e
Takeshi Suzuki <takeshibaconsuzuki@users.noreply.github.com>
2018-02-24 18:07:07 -0600

    Update drawing.py

---
+++ drawing.py
@@ -0,0 +1,2 @@
+def sriram():
+    print('once upon a time there was an old man. He toiled all day every day, 24/7 but still could not make enough for his children and his wife. One day, he aborted his 18 year old child and his wife, leaving the mentally scarred 12-year-old daughter. The protagonist was the girl. The end.')
